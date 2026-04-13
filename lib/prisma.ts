a556563294a00714c9e2749a5601f3af329b291c
rudraparekh04 <rudraparekh04@gmail.com>
2026-04-13 12:36:36 +0530

    add prisma accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,24 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+//   })
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client"
+import { withAccelerate } from "@prisma/extension-accelerate"
+
+const globalForPrisma = globalThis as any
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
+  new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
